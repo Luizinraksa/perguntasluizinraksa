aeb5758db24c9eb63bb06042dc0c8b48e598c6f9
luizin <117746938+Luizinraksa@users.noreply.github.com>
2024-08-30 18:23:50 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,143 @@
+export const perguntas = [
+    {
+        enunciado: "Qual você considera mais importante para a eficácia de uma estratégia de vendas?",
+        alternativas: [
+            {
+                texto: "Desenvolver técnicas de persuasão eficazes.",
+                afirmacao: [
+                    "Você valoriza o domínio das técnicas de persuasão como essencial para o sucesso em vendas.",
+                    "Acredita que a capacidade de persuadir é fundamental para maximizar o impacto da sua estratégia de vendas."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Manter uma abordagem ética e transparente.",
+                afirmacao: [
+                    "Você acredita que uma abordagem ética e transparente é crucial para a construção de uma relação de confiança com os clientes.",
+                    "Vê a ética e a transparência como a base para garantir uma estratégia de vendas sólida e confiável."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você valoriza o aprendizado das técnicas de persuasão, qual destas opções você acha mais útil?",
+        alternativas: [
+            {
+                texto: "Participar de workshops e treinamentos sobre vendas.",
+                afirmacao: [
+                    "Você acredita que workshops e treinamentos oferecem instruções detalhadas e aprimoram suas habilidades de vendas.",
+                    "Vê a formação especializada como uma forma eficaz de aprender e aplicar técnicas avançadas de persuasão."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Assistir a vídeos e tutoriais sobre técnicas de vendas.",
+                afirmacao: [
+                    "Você valoriza o uso de vídeos e tutoriais como uma maneira prática de aprender novas técnicas de vendas.",
+                    "Acredita que esses recursos oferecem uma forma acessível e flexível de aprimorar suas habilidades de persuasão."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acha que vídeos e tutoriais sobre técnicas de vendas são mais úteis, como você vê a importância do feedback presencial?",
+        alternativas: [
+            {
+                texto: "O feedback presencial fornece correção imediata e ajustes nas técnicas.",
+                afirmacao: [
+                    "Você vê o feedback presencial como uma forma importante de receber correção direta e ajustar possíveis erros nas técnicas de vendas.",
+                    "Acredita que a orientação ao vivo é fundamental para garantir a aplicação correta das técnicas de persuasão."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "O feedback presencial é menos relevante comparado à autonomia do aprendizado online.",
+                afirmacao: [
+                    "Você considera que a autonomia e o aprendizado autodidata têm mais impacto do que o feedback presencial.",
+                    "Vê o aprendizado online como mais flexível e adaptável às suas necessidades pessoais."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acredita que o feedback presencial é importante, como você avalia a importância de se manter informado sobre as tendências de mercado e técnicas de vendas?",
+        alternativas: [
+            {
+                texto: "Manter-se informado é crucial para adaptar as estratégias e evitar práticas obsoletas.",
+                afirmacao: [
+                    "Você vê a atualização constante como uma necessidade para adaptar as estratégias de vendas e evitar práticas obsoletas.",
+                    "Acredita que estar informado sobre novas tendências é vital para continuar evoluindo como profissional de vendas."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Manter-se informado é secundário em comparação com a prática regular de vendas.",
+                afirmacao: [
+                    "Você considera que a prática regular de vendas é mais importante do que estar atualizado com as últimas tendências.",
+                    "Acredita que a consistência e o foco são mais decisivos para alcançar o sucesso do que as novas tendências."
+                ],
+                proxima: 8,
+            },
+        ]
+    },
+    {
+        enunciado: "Se você considera importante manter-se informado, qual formato você acha mais eficaz para aprender sobre novas tendências e técnicas de vendas?",
+        alternativas: [
+            {
+                texto: "Participar de eventos e conferências do setor.",
+                afirmacao: [
+                    "Você vê eventos e conferências do setor como métodos eficazes para aprender diretamente sobre novas tendências e técnicas.",
+                    "Acredita que esses eventos oferecem oportunidades práticas e de networking no mercado de vendas."
+                ],
+            },
+            {
+                texto: "Leitura de artigos e publicações especializadas.",
+                afirmacao: [
+                    "Você valoriza a leitura de artigos e publicações como uma forma de se atualizar e aprofundar seu conhecimento de maneira autônoma.",
+                    "Acredita que esses recursos proporcionam uma visão detalhada e bem pesquisada sobre novas tendências e técnicas."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acha que a leitura de artigos é eficaz, como você vê a relação entre aprendizado autodidata e certificações em vendas?",
+        alternativas: [
+            {
+                texto: "O aprendizado autodidata pode ser tão eficaz quanto certificações formais, dependendo do contexto.",
+                afirmacao: [
+                    "Você considera que o aprendizado autodidata pode ser igualmente válido e útil, dependendo das circunstâncias e do contexto.",
+                    "Vê o autoestudo como uma abordagem flexível que pode complementar ou substituir certificações formais."
+                ],
+            },
+            {
+                texto: "Certificações formais são mais valiosas para garantir a qualidade e a validação do conhecimento adquirido.",
+                afirmacao: [
+                    "Você acredita que certificações formais oferecem um reconhecimento oficial e garantem a qualidade do conhecimento adquirido.",
+                    "Vê as certificações como uma forma importante de validar suas competências e habilidades no campo das vendas."
+                ],
+            },
+        ]
+    },
+    {
+        enunciado: "Se você acredita que certificações formais são mais valiosas, como você avalia a importância das habilidades interpessoais na interação com clientes e colegas de trabalho?",
+        alternativas: [
+            {
+                texto: "As habilidades interpessoais são essenciais para o sucesso e a integração com clientes e colegas.",
+                afirmacao: [
+                    "Você considera que habilidades interpessoais são fundamentais para a eficácia e a dinâmica ao interagir com clientes e colegas.",
+                    "Acredita que a capacidade de se comunicar e colaborar é tão importante quanto as qualificações técnicas."
+                ],
+            },
+            {
+                texto: "As habilidades interpessoais são importantes, mas as certificações e qualificações técnicas são mais decisivas para o sucesso.",
+                afirmacao: [
+                    "Você vê as certificações e qualificações técnicas como os principais fatores para o sucesso e a progressão na área de vendas.",
+                    "Acredita que, embora as habilidades interpessoais sejam valiosas, elas não superam a importância das certificações formais."
+                ],
+            },
+        ]
+    }
+];
